a751348e7158e2fb1bcfc00d78e0d080ce7c199e
mpratyush2016 <49927595+mpratyush2016@users.noreply.github.com>
2019-04-24 05:57:30 +0530

    Add files via upload
    the program of hello world

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, sum=0;
+
+    cout<<"Enter the first number";
+    cin>>a;
+    cout<<"Enter the second number";
+    cin>>b;
+    sum = a+b;
+    cout<<"The sum is = "<<sum;
+    return 0;
+}
